a728c1a3687f356397df459eb3a27302f3863178
Benjamin Newman <bnewman>
2026-01-13 19:23:21 -0500

    add layer abstraction

--- vbo_example.py
+++ vbo_example.py
@@ -24,11 +24,13 @@ void main()
 pygame.init()
 display = (800,600)
 pygame.display.set_mode (display, pygame.OPENGL|pygame.DOUBLEBUF, 24)
-glViewport (0, 0, 800, 600)
+glViewport (0, 0, *display)
 
 vertexShader = shaders.compileShader(VS, GL_VERTEX_SHADER)
 fragmentShader = shaders.compileShader(FS, GL_FRAGMENT_SHADER)
 shaderProgram = shaders.compileProgram(vertexShader, fragmentShader)
+
+glUseProgram(shaderProgram)
 
 glEnableClientState (GL_VERTEX_ARRAY)
 glEnableClientState (GL_COLOR_ARRAY)
@@ -59,50 +61,74 @@ vertsforedge = { e: [ facesforedge[e][0] + facesforedge[e][1] + f3 for f3 in
                         if tuple(f) in map(tuple, facesforface[tuple(facesforedge[e][1])]) ]
                      ] for e in facesforedge }
 
-triverts = []
-tricolors = []
+class layer():
+    def __init__(self):
+        self.triverts = []
+        self.tricolors = []
+        self.lineverts = []
+        self.linecolors = []
 
-lineverts = []
-linecolors = []
+        self.tridirty = False
+        self.linedirty = False
 
+        self.trivertbuf, self.tricolbuf, self.linevertbuf, self.linecolbuf = glGenBuffers(4)
+
+    def triangle(self, a,b,c, color):
+        self.tridirty = True
+
+        for v in (a,b,c):
+            self.triverts.append(v)
+            self.tricolors.append(color)
+
+    def line(self, a,b, color):
+        self.linedirty = True
+
+        for v in (a,b):
+            self.lineverts.append(v)
+            self.linecolors.append(color)
+
+    @staticmethod
+    def _bindbuffer(b, l):
+        glBindBuffer (GL_ARRAY_BUFFER, b)
+        data = numpy.concatenate(l, dtype=numpy.float32)
+        glBufferData (GL_ARRAY_BUFFER, data.nbytes, data, GL_STATIC_DRAW)
+
+    def draw(self):
+        if self.tridirty:
+            layer._bindbuffer(self.trivertbuf, self.triverts)
+            layer._bindbuffer(self.tricolbuf, self.tricolors)
+            self.tridirty = False
+
+        if self.linedirty:
+            layer._bindbuffer(self.linevertbuf, self.lineverts)
+            layer._bindbuffer(self.linecolbuf, self.linecolors)
+            self.linedirty = False
+
+        glBindBuffer(GL_ARRAY_BUFFER, self.trivertbuf)
+        glVertexPointer(3, GL_FLOAT, 0, None)
+
+        glBindBuffer (GL_ARRAY_BUFFER, self.tricolbuf)
+        glColorPointer (3, GL_FLOAT, 0, None)
+    
+        glDrawArrays (GL_TRIANGLES, 0, len(self.triverts))
+
+        glBindBuffer (GL_ARRAY_BUFFER, self.linevertbuf)
+        glVertexPointer (3, GL_FLOAT, 0, None)
+
+        glBindBuffer (GL_ARRAY_BUFFER, self.linecolbuf)
+        glColorPointer (3, GL_FLOAT, 0, None)
+    
+        glDrawArrays (GL_LINES, 0, len(self.lineverts))
+
+layers = [ layer() for i in range(1) ]
+        
 for f in faces:
     for e in edgesforface[tuple(f)]:
         for v in vertsforedge[tuple(e)]:
-            triverts.append(f)
-            triverts.append(e)
-            triverts.append(v)
-            for i in range(3):
-                tricolors.append(numpy.array([0,1,0]))
+            layers[0].triangle(f,e,v, numpy.array([0,1,0]))
 
 for e in vertsforedge:
-    lineverts.append(vertsforedge[e][0])
-    lineverts.append(vertsforedge[e][1])
-    for i in range(2):
-        linecolors.append(numpy.array([0,0,0]))
-
-print(len(lineverts))
-                
-tvbo = glGenBuffers (1)
-glBindBuffer (GL_ARRAY_BUFFER, tvbo)
-data = numpy.concatenate(triverts, dtype=numpy.float32)
-glBufferData (GL_ARRAY_BUFFER, data.nbytes, data, GL_STATIC_DRAW)
-
-tcbo = glGenBuffers (1)
-glBindBuffer (GL_ARRAY_BUFFER, tcbo)
-data = numpy.concatenate(tricolors, dtype=numpy.float32)
-glBufferData (GL_ARRAY_BUFFER, data.nbytes, data, GL_STATIC_DRAW)
-
-lvbo = glGenBuffers (1)
-glBindBuffer (GL_ARRAY_BUFFER, lvbo)
-data = numpy.concatenate(lineverts, dtype=numpy.float32)
-glBufferData (GL_ARRAY_BUFFER, data.nbytes, data, GL_STATIC_DRAW)
-
-lcbo = glGenBuffers (1)
-glBindBuffer (GL_ARRAY_BUFFER, lcbo)
-data = numpy.concatenate(linecolors, dtype=numpy.float32)
-glBufferData (GL_ARRAY_BUFFER, data.nbytes, data, GL_STATIC_DRAW)
-
-glUseProgram(shaderProgram)
+    layers[0].line(*vertsforedge[e], numpy.array([0,0,0]))
 
 running = True
 while running:
@@ -114,22 +140,9 @@ while running:
     glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glRotatef(1, 3, 1, 1)
 
-    glBindBuffer (GL_ARRAY_BUFFER, tvbo)
-    glVertexPointer (3, GL_FLOAT, 0, None)
-
-    glBindBuffer (GL_ARRAY_BUFFER, tcbo)
-    glColorPointer (3, GL_FLOAT, 0, None)
+    for l in layers:
+        l.draw()
     
-    glDrawArrays (GL_TRIANGLES, 0, 144)
-
-    glBindBuffer (GL_ARRAY_BUFFER, lvbo)
-    glVertexPointer (3, GL_FLOAT, 0, None)
-
-    glBindBuffer (GL_ARRAY_BUFFER, lcbo)
-    glColorPointer (3, GL_FLOAT, 0, None)
-    
-    glDrawArrays (GL_LINES, 0, 24)
-
     pygame.display.flip ()
     pygame.time.wait(10)
 
